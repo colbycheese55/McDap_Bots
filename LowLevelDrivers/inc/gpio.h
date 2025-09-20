cf9f6823ff92090a25cf196b3bf0e62039cbe0c3
Colby Wise <cwise1080@gmail.com>
2025-09-20 15:51:31 -0400

    gpio works

--- LowLevelDrivers/inc/gpio.h
+++ LowLevelDrivers/inc/gpio.h
@@ -6,33 +6,19 @@
 
 
 /**
- *  @brief      Read a GPIO pin (automatically detects input/output mode)
+ *  @brief      Read a GPIO *INPUT* pin)
  *
  *  @param[in]  gpio  Pointer to the register overlay for the peripheral
  *  @param[in]  pin   The GPIO pin to read. One of @ref DL_GPIO_PIN.
  *
  *  @return     The state of the GPIO pin (0 or 1).
- *              For input pins: reads the input register (DIN31_0)
- *              For output pins: reads the output register (DOUT31_0)
  */
 uint8_t gpio_read_pin(GPIO_Regs* gpio, uint32_t pin) {
-    // Check if pin is configured as output by reading DOE (Data Output Enable) register
-    uint32_t is_output = gpio->DOE31_0 & pin;
-    
-    if (is_output) {
-        // Pin is configured as output - read from output register
-        uint32_t mask = gpio->DOUT31_0 & pin;
-        return mask != 0 ? 1 : 0;
-    } else {
-        // Pin is configured as input - read from input register
-        uint32_t mask = gpio->DIN31_0 & pin;
-        return mask != 0 ? 1 : 0;
-    }
+    return (gpio->DIN31_0 & pin) ? 1 : 0;
 }
 
-
 /**
- *  @brief      Update the value of a GPIO pin
+ *  @brief      Update the value of a GPIO *OUTPUT* pin
  *
  *  @param[in]  gpio      Pointer to the register overlay for the peripheral
  *  @param[in]  pin       The GPIO pin you want to update. One of @ref DL_GPIO_PIN.
@@ -44,6 +30,16 @@ void gpio_write_pin(GPIO_Regs* gpio, uint32_t pin, uint8_t val) {
     } else {
         gpio->DOUTCLR31_0 = pin;
     }
+}
+
+/**
+ *  @brief      Toggle a GPIO *OUTPUT* pin
+ *
+ *  @param[in]  gpio  Pointer to the register overlay for the peripheral
+ *  @param[in]  pin   The GPIO pin to toggle. One of @ref DL_GPIO_PIN.
+ */
+void gpio_toggle_pin(GPIO_Regs* gpio, uint32_t pin) {
+    gpio->DOUTTGL31_0 = pin;
 }
 
 #endif // GPIO_H
