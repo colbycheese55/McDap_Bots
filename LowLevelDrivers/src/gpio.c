45ea3469ed9ea34eda7b82391d33296a446ed5ec
DavidMe03 <davidmerino407@gmail.com>
2025-10-28 15:13:00 -0400

    quick TI gpio fix

--- LowLevelDrivers/src/gpio.c
+++ LowLevelDrivers/src/gpio.c
@@ -2,11 +2,13 @@
 #include <ti/devices/msp/msp.h>
 #include <ti/driverlib/dl_common.h>
 
-uint8_t gpio_read_pin(GPIO_Regs* gpio, uint32_t pin) {
+uint8_t gpio_read_pin(GPIO_Regs* port, uint32_t pin) {
+    GPIO_Regs *gpio = (GPIO_Regs *)port;
     return (gpio->DIN31_0 & pin) ? 1 : 0;
 }
 
-void gpio_write_pin(GPIO_Regs* gpio, uint32_t pin, uint8_t val) {
+void gpio_write_pin(GPIO_Regs* port, uint32_t pin, uint8_t val) {
+    GPIO_Regs *gpio = (GPIO_Regs *)port;
     if (val) {
         gpio->DOUTSET31_0 = pin;
     } else {
@@ -14,6 +16,7 @@ void gpio_write_pin(GPIO_Regs* gpio, uint32_t pin, uint8_t val) {
     }
 }
 
-void gpio_toggle_pin(GPIO_Regs* gpio, uint32_t pin) {
+void gpio_toggle_pin(GPIO_Regs* port, uint32_t pin) {
+    GPIO_Regs *gpio = (GPIO_Regs *)port;
     gpio->DOUTTGL31_0 = pin;
 }
