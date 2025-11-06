aa4d423b2c41504e58a379a606978cd946f7dbb7
DavidMe03 <davidmerino407@gmail.com>
2025-11-06 13:24:20 -0500

    STM pwm.c code

---
+++ LowLevelDrivers/STM_src/pwm.c
@@ -0,0 +1,50 @@
+#include "pwm.h"
+
+static uint32_t clamp_cc(uint32_t period, float duty) {
+    if (duty < 0.f) duty = 0.f;
+    if (duty > 1.f) duty = 1.f;
+    uint32_t cmp = (uint32_t)(period * duty + 0.5f);
+    if (cmp > period) cmp = period;
+    return cmp;
+}
+
+static inline void set_cc(PWM_Handle *h, uint32_t cmp) {
+    __HAL_TIM_SET_COMPARE((TIM_HandleTypeDef*)h->peripheral,
+                          h->ccChannel,
+                          cmp);
+}
+
+void PWM_init(PWM_Handle *h,
+              void *peripheral,
+              uint8_t ccChannel,
+              uint32_t clkHz,
+              uint32_t pwmHz,
+              float duty)
+{
+    if (pwmHz == 0) pwmHz = 1;
+
+    h->peripheral = peripheral;
+    h->ccChannel  = ccChannel;
+    h->clkHz      = clkHz;
+    h->period     = clkHz / pwmHz;
+    h->duty       = duty;
+
+    // Set ARR
+    __HAL_TIM_SET_AUTORELOAD((TIM_HandleTypeDef*)peripheral, h->period);
+
+    // Set CCR
+    set_cc(h, clamp_cc(h->period, duty));
+}
+
+void PWM_setDuty(PWM_Handle *h, float duty) {
+    h->duty = duty;
+    set_cc(h, clamp_cc(h->period, duty));
+}
+
+void PWM_setFrequency(PWM_Handle *h, uint32_t pwmHz) {
+    if (pwmHz == 0) pwmHz = 1;
+    h->period = h->clkHz / pwmHz;
+
+    __HAL_TIM_SET_AUTORELOAD((TIM_HandleTypeDef*)h->peripheral, h->period);
+    PWM_setDuty(h, h->duty);
+}
