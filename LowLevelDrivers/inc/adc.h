8b62132dd9de1d154d9a1aa70180ef6ed8b2172d
DavidMe03 <davidmerino407@gmail.com>
2025-11-13 15:15:38 -0500

    cleaned up adc interface

--- LowLevelDrivers/inc/adc.h
+++ LowLevelDrivers/inc/adc.h
@@ -24,16 +24,6 @@ typedef struct {
 
 void init_adc(ADC_Handle* adc, size_t adc_ptr, uint32_t channel);
 
-// begin converting an analog input to digital
-void adc_start_conversion(ADC_Handle *adc);
-
-// read the ADC memory to get the digital value. Does NOT wait for conversion to complete
-uint16_t adc_get_memory_result(ADC_Handle *adc);
-
-// reenable the ADC after completing the conversion
-void adc_enable_conversions(ADC_Handle *adc);
-
-
 // read the ADC for the current analog value, returns a float between 0.0 and 1.0
 float adc_get_value(ADC_Handle *adc);
 
