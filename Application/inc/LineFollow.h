ee96a28e33c5c4dbfec7df352c34f7357df15ed2
Colby Wise <cwise1080@gmail.com>
2025-12-12 22:46:00 -0500

    added speed reduction and multisampling to LineFollow

--- Application/inc/LineFollow.h
+++ Application/inc/LineFollow.h
@@ -1,5 +1,5 @@
 
-#define LINE_FOLLOW_DISABLE     1 // disable if the reflection sensor is not connected
+#define LINE_FOLLOW_DISABLE     0 // disable if the reflection sensor is not connected
 
 // blocking function that runs the line following application
 void line_follow_run();
--- Application/src/LineFollow.c
+++ Application/src/LineFollow.c
@@ -1,4 +1,6 @@
 #include <stdint.h>
+#include <stdlib.h>
+#include <math.h>
 #include "../inc/LineFollow.h"
 #include "../inc/ApplicationSwitcher.h"
 #include "../../HighLevelDrivers/inc/reflectance_sensor.h"
@@ -6,8 +8,9 @@
 
 #define MIDPONT     7
 #define P           0.10 // TODO
-#define D           0.0 // TODO
-#define BASE_SPEED  0.2f
+#define D           0.05 // TODO
+#define BASE_SPEED  0.3f
+#define NUM_SAMPLES 5
 
 int8_t prev_error = 0;
 
@@ -30,14 +33,30 @@ uint8_t get_right_offset(uint16_t reading) {
     return count;
 }
 
+int compare_ints(const void *a, const void *b) {
+    return (*(uint8_t*)a - *(uint8_t*)b);
+}
+
+uint8_t get_median(uint8_t* array, uint8_t length) {
+    qsort(array, length, sizeof(uint8_t), compare_ints);
+    return array[length / 2];
+}
+
 
 void line_follow_run() {
     while (!application_yield) {
         // read the sensor, compute error and correction
-        uint16_t sensor_reading = refsen_read_values();
+        uint8_t left_offsets[NUM_SAMPLES];
+        uint8_t right_offsets[NUM_SAMPLES];
+        for (uint8_t i = 0; i < NUM_SAMPLES; i++) {
+            uint16_t sensor_reading = refsen_read_values();
 
-        uint8_t left_offset = get_left_offset(sensor_reading);
-        uint8_t right_offset = get_right_offset(sensor_reading);
+            left_offsets[i] = get_left_offset(sensor_reading);
+            right_offsets[i] = get_right_offset(sensor_reading);
+        }
+
+        uint8_t left_offset = get_median(left_offsets, NUM_SAMPLES);
+        uint8_t right_offset = get_median(right_offsets, NUM_SAMPLES);
 
         int8_t error = (MIDPONT - left_offset) - (MIDPONT - right_offset); // positive is right error, negative is left error
 
@@ -50,10 +69,10 @@ void line_follow_run() {
         // dynamic velocity scaling based on error
         float translational_speed = BASE_SPEED;
         float angular_speed = correction * BASE_SPEED;
-        // if (error > 1) {
-        //     translational_speed /= error;
-        //     angular_speed /= error;
-        // }
+        if (error > 1) {
+            translational_speed /= pow(error, 0.33);
+            angular_speed /= pow(error, 0.33);
+        }
 
         drive_arc(translational_speed, angular_speed);
     }
